c50a447953175960c57a10d06d8620bc3915309a
sweena898 <sweenaa96@gmail.com>
2024-12-11 14:06:47 +0530

    update

---
+++ Add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Add
+{
+public static void main(String args[])
+{
+Scanner myobj=new Scanner(System.in);
+System.out.print("Enter any number:");
+int first=myobj.nextInt();
+System.out.print("Enter any number:");
+int second=myobj.nextInt();
+int add=first+second;
+System.out.println("The sum is:"+add);
+}
+}
